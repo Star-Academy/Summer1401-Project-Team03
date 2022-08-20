f3d2fe0cf3aafd088749b8771f933e9597478eb8
KhosroMoeini <KhosroMoeini2003@gmail.com>
2022-08-20 14:49:15 +0430

    Update IDatabase.cs

--- server/Database/IDatabase.cs
+++ server/Database/IDatabase.cs
@@ -2,6 +2,7 @@
 
 public interface IDatabase
 {
+    public void Execute(string query);
     public string Join(string firstTable, string secondTable);
     public string CreateTable(string tableName, List<string> keys);
     /*TODO
