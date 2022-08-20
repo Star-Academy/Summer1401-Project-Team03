eef2974f10c5f6b4a7baeee35d7d64dd969820a0
AliZoghi <alizoghiwork@gmail.com>
2022-08-20 17:00:27 +0430

    [ADD] svg component

--- client/src/app/components/navbar/navbar.module.ts
+++ client/src/app/components/navbar/navbar.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NavbarComponent} from './navbar.component';
 import { NavbarItemComponent } from './components/navbar-item/navbar-item.component';
+import {SvgModule} from "../svg/svg.module";
 
 @NgModule({
     declarations: [NavbarComponent, NavbarItemComponent, NavbarItemComponent],
-  imports: [CommonModule],
+    imports: [CommonModule, SvgModule],
   exports: [
     NavbarComponent
   ]
