7740b963a85c25a3ae33c619f71c31018605da6f
AliZoghi <alizoghiwork@gmail.com>
2022-08-20 22:30:07 +0430

    [FIX] pr issue, change fucntion name

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
     public shrinkedNavbar: boolean = false;
-    public shrinkNavbar(): void {
+    public toggleShrink(): void {
         this.shrinkedNavbar = !this.shrinkedNavbar;
     }
 }
