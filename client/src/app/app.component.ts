8aacbdae43fe5ae97118280da7987418617f4d80
Negarnbkhti42 <norob35@gmail.com>
2022-08-21 11:43:04 +0430

    [ADD] upload modal

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {ModalComponent} from './components/modal/modal.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+    @ViewChild('mymodal') public modal!: ModalComponent;
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {ModalModule} from './components/modal/modal.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, ModalModule],
     providers: [],
     bootstrap: [AppComponent],
 })
