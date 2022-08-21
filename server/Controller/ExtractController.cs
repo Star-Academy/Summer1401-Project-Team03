d42858e8d2e72889532d9fdd4c5cfecf929d7c87
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2022-08-21 16:22:24 +0430

    Add import api

--- server/Controller/ExtractController.cs
+++ server/Controller/ExtractController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DefaultNamespace;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Extensions;
+using server.Extract;
 
 namespace server.Controller;
 
-public class ExtractController : ControllerBase
+[ApiController]
+[Route("[controller]/[Action]")]
+public class FileTransferController : ControllerBase
 {
+    private static int _fileID = 0;
     [HttpPost]
-    public IActionResult Extract()
+    public async Task<IActionResult> Import(IFormFile file)
     {
-        return Ok();
+        if (file.Length > 0)
+        {
+            increaseFileID(1);
+            var filePath = Environment.CurrentDirectory + "\\resources" + "\\" + file.FileName + _fileID;
+            
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            switch (file.ContentType)
+            {
+                case nameof(FileTypes.csv):
+                    new CSVExtractor(filePath, file.FileName).Extract();
+                    break;
+                case nameof(FileTypes.json):
+                    new JSONExtractor(filePath, file.FileName).Extract();
+                    break;
+            }
+            
+            return Ok();
+        }
+
+        return BadRequest("The sent file is empty!");
+    }
+
+    // [HttpGet]
+    // public ActionResult<IFormFile> Export()
+    // {
+    //     
+    // }
+
+    private void increaseFileID(int increament)
+    {
+        _fileID += increament;
     }
 }
