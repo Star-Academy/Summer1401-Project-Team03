19fd59989b30544d89105b90c69ee85fee03f615
Negarnbkhti42 <norob35@gmail.com>
2022-08-22 17:33:27 +0430

    [FIX] test configurations

--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
-
-    public value: boolean = false;
-}
+export class DashboardComponent {}
--- client/src/app/pages/dashboard/dashboard.module.ts
+++ client/src/app/pages/dashboard/dashboard.module.ts
@@ -2,10 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './dashboard.component';
 import {RouterModule} from '@angular/router';
-import {SwitchModule} from 'src/app/components/switch/switch.module';
 
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [CommonModule, RouterModule, SwitchModule],
+    imports: [CommonModule, RouterModule],
 })
 export class DashboardModule {}
