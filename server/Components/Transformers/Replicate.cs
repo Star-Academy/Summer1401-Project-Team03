1dbb19ca91d69698f79276ca59d4d915226fee13
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2022-08-22 16:21:04 +0430

    changes

--- server/Components/Transformers/Replicate.cs
+++ server/Components/Transformers/Replicate.cs
@@ -3,14 +3,31 @@ using server.Components.Transformers;
 
 namespace server.Transform;
 
-public class Replicate : Transformer
+public class Replicate : Mutator
 {
     public List<string> Keys { get; set; }
+    private int counter = 0;
+    private int _branches = 2;
+    private string previousTableName;
 
     public List<Component> PreviousComponents { get; set; }
+    public override void Mutate()
+    {
+        pipeline.QueryBuilder.CopyTable(PreviousComponents[0].GetTableName(), previousTableName);
+        throw new NotImplementedException();
+    }
 
     public override string GetQuery()
     {
+        previousTableName = PreviousComponents[0].GetTableName();
+        counter = counter == _branches ? 1 : counter + 1;
         return PreviousComponents[0].GetQuery();
     }
+    
+    public override string GetTableName()
+    {
+        return $"{previousTableName}{counter}";
+    }
+
+    
 }
