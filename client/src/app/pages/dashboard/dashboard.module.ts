60ab82d597b2449f6556643a1af42f736782cc99
Negarnbkhti42 <norob35@gmail.com>
2022-08-22 11:29:49 +0430

    [FIX] switch color

--- client/src/app/pages/dashboard/dashboard.module.ts
+++ client/src/app/pages/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './dashboard.component';
 import {RouterModule} from '@angular/router';
+import {SwitchModule} from 'src/app/components/switch/switch.module';
 
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, SwitchModule],
 })
 export class DashboardModule {}
