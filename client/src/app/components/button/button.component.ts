260a5d892c58de1ebea7a26112ce495c6628e06e
Negarnbkhti42 <norob35@gmail.com>
2022-08-22 10:06:15 +0430

    [ADD] component button

---
+++ client/src/app/components/button/button.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+    selector: 'app-button',
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {
+    @Input() public type: string = 'button';
+    @Input() public disabled: boolean = false;
+}
