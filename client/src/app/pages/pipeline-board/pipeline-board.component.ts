b4fe33d5804b9ddca43e158d37a6949ffc78c7a8
AliZoghi <alizoghiwork@gmail.com>
2022-08-22 20:24:29 +0430

    [ADD] ngx-draggabilly library

--- client/src/app/pages/pipeline-board/pipeline-board.component.ts
+++ client/src/app/pages/pipeline-board/pipeline-board.component.ts
@@ -1,8 +1,13 @@
 import {Component, OnInit} from '@angular/core';
+import {NgxDraggabillyOptions} from 'ngx-draggabilly';
 
 @Component({
     selector: 'app-pipeline-board',
     templateUrl: './pipeline-board.component.html',
     styleUrls: ['./pipeline-board.component.scss'],
 })
-export class PipelineBoardComponent {}
+export class PipelineBoardComponent {
+    public draggabillyOptions: NgxDraggabillyOptions = {
+        containment: true,
+    };
+}
--- client/src/app/pages/pipeline-board/pipeline-board.module.ts
+++ client/src/app/pages/pipeline-board/pipeline-board.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PipelineBoardComponent} from './pipeline-board.component';
 import { PipelineNodeComponent } from './components/pipeline-node/pipeline-node.component';
+import {NgxDraggabillyModule} from "ngx-draggabilly";
 
 @NgModule({
     declarations: [PipelineBoardComponent, PipelineNodeComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, NgxDraggabillyModule],
 })
 export class PipelineBoardModule {}
