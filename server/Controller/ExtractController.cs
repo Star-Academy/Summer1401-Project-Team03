000bd97827b692dda8ffe69aaf679c45f2006edf
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2022-08-22 08:24:53 +0430

    Update ExtractController.cs

--- server/Controller/ExtractController.cs
+++ server/Controller/ExtractController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using DefaultNamespace;
+using Microsoft.AspNetCore.Mvc;
+using server.Extract;
 
 namespace server.Controller;
 
-public class ExtractController : ControllerBase
+[ApiController]
+[Route("[controller]/[Action]")]
+public class FileTransferController : ControllerBase
 {
+    private static int _fileID = 0;
     [HttpPost]
-    public IActionResult Extract()
+   /* public async Task<IActionResult> Import(IFormFile file)
     {
-        return Ok();
+        if (file.Length > 0)
+        {
+            increaseFileID(1);
+            var filePath = Environment.CurrentDirectory + "\\resources" + "\\" + file.FileName + "_" + _fileID;
+            
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            switch (file.ContentType)
+            {
+                case nameof(FileTypes.csv):
+                    new CSVExtractor(filePath, file.FileName).Extract();
+                    break;
+                case nameof(FileTypes.json):
+                    new JSONExtractor(filePath, file.FileName).Extract();
+                    break;
+            }
+            
+            return Ok();
+        }
+
+        return BadRequest("The sent file is empty!");
+    }*/
+
+     [HttpGet]
+     public ActionResult<VirtualFileResult> Export(string fileName, int? fileID)
+     {
+         var filePath = Environment.CurrentDirectory + "\\resources" + "\\" + fileName + "_" + fileID;
+         //return File(System.IO.File.Open(filePath, FileMode.Open));
+         
+         return File(filePath, "text/plain"); 
+     }
+
+    private void increaseFileID(int increament)
+    {
+        _fileID += increament;
     }
 }
