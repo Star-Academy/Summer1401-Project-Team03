69f307eb1cff551bc47ee55c207f14994a0fc9dd
Negarnbkhti42 <norob35@gmail.com>
2022-08-22 17:34:44 +0430

    [FIX] test configurations

--- client/src/app/components/navbar/navbar.module.ts
+++ client/src/app/components/navbar/navbar.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NavbarComponent} from './navbar.component';
-import { NavbarItemComponent } from './components/navbar-item/navbar-item.component';
-import {SvgModule} from "../svg/svg.module";
+import {NavbarItemComponent} from './components/navbar-item/navbar-item.component';
+import {SvgModule} from '../svg/svg.module';
 
 @NgModule({
     declarations: [NavbarComponent, NavbarItemComponent, NavbarItemComponent],
     imports: [CommonModule, SvgModule],
-  exports: [
-    NavbarComponent
-  ]
+    exports: [NavbarComponent],
 })
 export class NavbarModule {}
--- client/src/app/components/svg/svg.component.spec.ts
+++ client/src/app/components/svg/svg.component.spec.ts
@@ -1,25 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SvgComponent } from './svg.component';
+import {SvgComponent} from './svg.component';
 
 describe('SvgComponent', () => {
-  let component: SvgComponent;
-  let fixture: ComponentFixture<SvgComponent>;
+    let component: SvgComponent;
+    let fixture: ComponentFixture<SvgComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SvgComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SvgComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SvgComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SvgComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
