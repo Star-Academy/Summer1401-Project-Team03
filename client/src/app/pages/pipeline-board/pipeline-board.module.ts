d61f965729b9ce9b8a2a2e7c1c54c64e526a16b6
AliZoghi <alizoghiwork@gmail.com>
2022-08-23 09:02:47 +0430

    [ADD] Angular2-Draggable library

--- client/src/app/pages/pipeline-board/pipeline-board.module.ts
+++ client/src/app/pages/pipeline-board/pipeline-board.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PipelineBoardComponent} from './pipeline-board.component';
-import { PipelineNodeComponent } from './components/pipeline-node/pipeline-node.component';
-import {NgxDraggabillyModule} from "ngx-draggabilly";
-import {SvgModule} from "../../components/svg/svg.module";
+import {PipelineNodeComponent} from './components/pipeline-node/pipeline-node.component';
+import {NgxDraggabillyModule} from 'ngx-draggabilly';
+import {SvgModule} from '../../components/svg/svg.module';
+import {AngularDraggableModule} from 'angular2-draggable';
 
 @NgModule({
     declarations: [PipelineBoardComponent, PipelineNodeComponent],
-    imports: [CommonModule, NgxDraggabillyModule, SvgModule],
+    imports: [CommonModule, NgxDraggabillyModule, SvgModule, AngularDraggableModule],
 })
 export class PipelineBoardModule {}
