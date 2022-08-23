8bd4da97cb14a125374c957d6f6fb50d19f35f67
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2022-08-23 17:51:39 +0430

    fixing hash

--- server/Components/Transformers/Hash.cs
+++ server/Components/Transformers/Hash.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Data.Common;
 using System.Security.Cryptography;
 using System.Text;
 using server.Pipelines;
@@ -6,43 +7,52 @@ using SqlKata;
 
 namespace server.Components.Transformers;
 
-public class Hash : Transformer
+public class Hash : Mutator
 {
-    public Hash(Pipeline pipeline, Position position) : base(pipeline, position)
+    public Hash(Pipeline pipeline, Position position, string fieldToHash, bool shouldCreateNewField, string newFieldName) : base(pipeline, position)
     {
+        FieldToHash = fieldToHash;
+        NewFieldName = newFieldName;
+        ShouldCreateNewField = shouldCreateNewField;
+
     }
 
-    private string fieldToHash { get; set; }
+    private string FieldToHash { get; set; }
     private bool ShouldCreateNewField { get; set; }
-    private string newFieldName { get; set; }
+    private string NewFieldName { get; set; }
 
-
+    public override void Mutate()
+    {
+        
+        var previousTableName = PreviousComponents[0].GetQuery();
+        Pipeline.QueryBuilder.Copy(TableName, previousTableName);
+    }
+    
     public override string GetQuery()
     {
-        var query = new Query(PreviousComponents[0].GetQuery());
+        TableName = Pipeline.TableManager.NewTableName();
+        
+        return Pipeline.QueryBuilder.SelectTable(TableName);
+    }
 
-        //TODO read table as data
-        dynamic data = query;
+    private List<string> GetHashedValueOfColumnn(string previousTableName)
+    {
+        var tempName = Pipeline.TableManager.NewTableName();
+        var query = Pipeline.QueryBuilder.Select(new List<string> {FieldToHash}, previousTableName,
+            tempName);
 
-        var hashValues = new List<string>();
-
-        var a = ((IEnumerable) data).Cast<dynamic>();
-
-        foreach (var o in a)
-        {
-            var dynamicController = new DynamicObjectController(o);
-            hashValues.Add(GetHashString(dynamicController.GetDynamicMember(fieldToHash)));
-        }
-
-        throw new NotImplementedException();
+        var reader = Pipeline.Database.Execute(query);
+        var hashedValues = CreateListOfStringsFromReader(reader, 0)
+                                    .Select(GetHashString).ToList();
+        reader.Close();
+        
+        return hashedValues;
     }
 
     private IEnumerable<byte> GetHashArray(string str)
     {
-        using (HashAlgorithm algorithm = SHA256.Create())
-        {
-            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(str));
-        }
+        using HashAlgorithm algorithm = SHA256.Create();
+        return algorithm.ComputeHash(Encoding.UTF8.GetBytes(str));
     }
 
     private string GetHashString(object obj)
@@ -52,5 +62,33 @@ public class Hash : Transformer
             sb.Append(b.ToString("X2"));
 
         return sb.ToString();
+    }
+
+    private string SqlHash()
+    {
+        return @"create function h_int(text) returns int as $$
+        select ('x'||substr(md5($1),1,8))::bit(32)::int;
+            $$ language sql";
+    }
+
+
+    public IEnumerable<string> CreateListOfStringsFromReader(DbDataReader reader, int ordinal)
+    {
+        var strings = new List<string>();
+
+        if (!reader.HasRows) return strings;
+        while (reader.Read())
+        {
+            try
+            {
+                strings.Add(reader.GetString(ordinal));
+            }
+            catch (InvalidCastException)
+            {
+                strings.Add("0");
+            }
+        }
+
+        return strings;
     }
 }
