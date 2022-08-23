e40635fbeb7c2f209309aaa1da5acb54afa7df1b
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-23 12:11:59 +0430

    [ADD] basic topbar

--- client/src/app/pages/pipeline/pipeline.module.ts
+++ client/src/app/pages/pipeline/pipeline.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PipelineComponent} from './pipeline.component';
 import {BoardComponent} from './components/board/board.component';
+import {SvgModule} from '../../components/svg/svg.module';
+import {RunButtonComponent} from './components/run-button/run-button.component';
 
 @NgModule({
-    declarations: [PipelineComponent, BoardComponent],
-    imports: [CommonModule],
+    declarations: [PipelineComponent, BoardComponent, RunButtonComponent],
+    imports: [CommonModule, SvgModule],
 })
 export class PipelineModule {}
