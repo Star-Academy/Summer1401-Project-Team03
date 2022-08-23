8defb0af0245e815d2ce0329c642f615b6ecbcca
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2022-08-23 17:54:37 +0430

    adding constructor to DBconfiguration

--- server/configurations/DBConfiguration.cs
+++ server/configurations/DBConfiguration.cs
@@ -2,6 +2,14 @@
 
 public record DBConfiguration
 {
+    public DBConfiguration(string host, string username, string database, string password)
+    {
+        Host = host;
+        Username = username;
+        Database = database;
+        Password = password;
+    }
+
     public string Host { get; set; }
     public string Username { get; set; }
     public string Database { get; set; }
