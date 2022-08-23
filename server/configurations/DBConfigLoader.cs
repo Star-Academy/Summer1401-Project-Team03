22744602db33b1db2cb7c62259e305563cb5508e
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2022-08-23 11:12:27 +0430

    Add DBconfiguration

---
+++ server/configurations/DBConfigLoader.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using server.configurations;
+
+namespace a;
+
+public static class DBConfigLoader
+{
+    public static DBConfiguration Load()
+    {
+        using (StreamReader r = new StreamReader("configurations/DBconfig.json"))
+        {
+            string json = r.ReadToEnd();
+            var dbConfiguration = JsonConvert.DeserializeObject<DBConfiguration>(json);
+            return dbConfiguration;
+        }
+    }
+}
---
+++ server/configurations/DBConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace server.configurations;
+
+public record DBConfiguration
+{
+    public string Host { get; set; }
+    public string Username { get; set; }
+    public string Database { get; set; }
+    public string Password { get; set; }
+}
