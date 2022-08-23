26fb7c8cc44884e4f16b4254f911c085d033b4d7
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2022-08-23 12:20:12 +0430

    update pipeline

--- server/Program.cs
+++ server/Program.cs
@@ -1,61 +1,61 @@
-// var builder = WebApplication.CreateBuilder(args);
-//
-// // Add services to the container.
-//
-// builder.Services.AddControllers();
-// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-//
-// builder.Services.AddSwaggerGen();
-//
-// var app = builder.Build();
-//
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-//
-// app.UseHttpsRedirection();
-//
-// app.UseAuthorization();
-//
-// app.MapControllers();
-//
-// app.Run();
+var builder = WebApplication.CreateBuilder(args);
 
-using a;
-using server.Components.Extractors;
-using server.Components.Loaders;
-using server.Components.Transformers;
-using server.Enums;
-using server.Pipelines;
+// Add services to the container.
 
-public static class Program
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var dbConfiguration = DBConfigLoader.Load();
-        var pipeline = new Pipeline(dbConfiguration.Host, dbConfiguration.Username, dbConfiguration.Database,
-            dbConfiguration.Password);
-        var extractor = new CSVExtractor(pipeline,
-            @"E:\input.csv");
-
-        var filter1 = new Filter(pipeline, "location", Operator.Equal, "Iran");
-        var filter2 = new Filter(pipeline, "date", Operator.Equal, "2022-08-12");
-
-        var loader = new CSVLoader(pipeline, filter2,
-            @"E:\output.csv");
-
-        filter1.PreviousComponents.Add(extractor);
-        filter2.PreviousComponents.Add(filter1);
-
-        pipeline.AddComponent(extractor);
-        pipeline.AddComponent(filter1);
-        pipeline.AddComponent(filter2);
-        pipeline.AddComponent(loader);
-
-        pipeline.Execute(3);
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+// using a;
+// using server.Components.Extractors;
+// using server.Components.Loaders;
+// using server.Components.Transformers;
+// using server.Enums;
+// using server.Pipelines;
+//
+// public static class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//         var dbConfiguration = DBConfigLoader.Load();
+//         var pipeline = new Pipeline(dbConfiguration.Host, dbConfiguration.Username, dbConfiguration.Database,
+//             dbConfiguration.Password);
+//         var extractor = new CSVExtractor(pipeline,
+//             @"E:\input.csv");
+//
+//         var filter1 = new Filter(pipeline, "location", Operator.Equal, "Iran");
+//         var filter2 = new Filter(pipeline, "date", Operator.Equal, "2022-08-12");
+//
+//         var loader = new CSVLoader(pipeline, filter2,
+//             @"E:\output.csv");
+//
+//         filter1.PreviousComponents.Add(extractor);
+//         filter2.PreviousComponents.Add(filter1);
+//
+//         pipeline.AddComponent(extractor);
+//         pipeline.AddComponent(filter1);
+//         pipeline.AddComponent(filter2);
+//         pipeline.AddComponent(loader);
+//
+//         pipeline.Execute(3);
+//     }
+// }
