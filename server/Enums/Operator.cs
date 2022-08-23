d118139bebd4d5f23a1efc59467a4a18471b5219
Ali Emamzadeh <ali.emamzadeh12@gmail.com>
2022-08-23 15:48:30 +0430

    add addTransformerController

--- server/Enums/Operator.cs
+++ server/Enums/Operator.cs
@@ -25,4 +25,18 @@ public static class Extensions
             _ => throw new ArgumentOutOfRangeException(nameof(@operator), @operator, null)
         };
     }
+
+    public static Operator GetOperator(this string @string)
+    {
+        return @string switch
+        {
+            "==" => Operator.Equal,
+            "!=" => Operator.NotEqual,
+            ">" => Operator.Greater,
+            ">=" => Operator.GreaterOrEqual,
+            "<" => Operator.Less,
+            "<=" => Operator.LessOrEqual,
+            _ => throw new ArgumentOutOfRangeException(nameof(@string), @string, null)
+        };
+    }
 }
