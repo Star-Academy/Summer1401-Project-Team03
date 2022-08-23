d984226d6d80422d0164371cd03d2b7c20f8b98c
KhosroMoeini <KhosroMoeini2003@gmail.com>
2022-08-23 13:20:36 +0430

    FieldSelector Added

---
+++ server/Components/Transformers/FieldSelector.cs
@@ -0,0 +1,23 @@
+﻿using server.Pipelines;
+
+namespace server.Components.Transformers;
+
+public class FieldSelector : Transformer
+{
+    public List<string> FieldsToSelect;
+
+    public FieldSelector(Pipeline pipeline, List<string> fieldsToSelect) : base(pipeline)
+    {
+        FieldsToSelect = fieldsToSelect;
+    }
+
+    public override string GetQuery()
+    {
+        return Pipeline.QueryBuilder.Select(GetKeys(), PreviousComponents[0].GetQuery(), Pipeline.TableManager.NewTableName());
+    }
+
+    public override List<string> GetKeys()
+    {
+        return FieldsToSelect;
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,61 +1,60 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-// using a;
-// using server.Components.Extractors;
-// using server.Components.Loaders;
-// using server.Components.Transformers;
-// using server.Enums;
-// using server.Pipelines;
+// var builder = WebApplication.CreateBuilder(args);
 //
-// public static class Program
+// // Add services to the container.
+//
+// builder.Services.AddControllers();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+//
+// builder.Services.AddSwaggerGen();
+//
+// var app = builder.Build();
+//
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
 // {
-//     public static void Main(string[] args)
-//     {
-//         var dbConfiguration = DBConfigLoader.Load();
-//         var pipeline = new Pipeline(dbConfiguration.Host, dbConfiguration.Username, dbConfiguration.Database,
-//             dbConfiguration.Password);
-//         var extractor = new CSVExtractor(pipeline,
-//             @"E:\input.csv");
-//
-//         var filter1 = new Filter(pipeline, "location", Operator.Equal, "Iran");
-//         var filter2 = new Filter(pipeline, "date", Operator.Equal, "2022-08-12");
-//
-//         var loader = new CSVLoader(pipeline, filter2,
-//             @"E:\output.csv");
-//
-//         filter1.PreviousComponents.Add(extractor);
-//         filter2.PreviousComponents.Add(filter1);
-//
-//         pipeline.AddComponent(extractor);
-//         pipeline.AddComponent(filter1);
-//         pipeline.AddComponent(filter2);
-//         pipeline.AddComponent(loader);
-//
-//         pipeline.Execute(3);
-//     }
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
 // }
+//
+// app.UseHttpsRedirection();
+//
+// app.UseAuthorization();
+//
+// app.MapControllers();
+//
+// app.Run();
+
+using a;
+using server.Components.Extractors;
+using server.Components.Loaders;
+using server.Components.Transformers;
+using server.Enums;
+using server.Pipelines;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var dbConfiguration = DBConfigLoader.Load();
+        var pipeline = new Pipeline(dbConfiguration.Host, dbConfiguration.Username, dbConfiguration.Database,
+            dbConfiguration.Password);
+        var extractor = new CSVExtractor(pipeline,
+            @"C:\Users\Khosro\Desktop\StarAcademy\Summer1401-Project-Team03\server\TestData\input.csv");
+
+        // var filter1 = new Filter(pipeline, "location", Operator.Equal, "Iran");
+        // filter1.PreviousComponents.Add(extractor);
+        // var filter2 = new Filter(pipeline, "date", Operator.Equal, "2022-08-12");
+        // filter2.PreviousComponents.Add(filter1);
+
+        var selector = new FieldSelector(pipeline,new List<string>(){"iso_code", "location"});
+        selector.PreviousComponents.Add(extractor);
+        
+        var loader = new CSVLoader(pipeline, selector,
+            @"C:\Users\Khosro\Desktop\StarAcademy\Summer1401-Project-Team03\server\TestData\output.csv");
+        
+        pipeline.AddComponent(loader);
+
+        pipeline.Execute(2);
+    }
+}
