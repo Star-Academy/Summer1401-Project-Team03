1f365f313f44c2feecb282e0adf74266e26d1f22
Negarnbkhti42 <norob35@gmail.com>
2022-08-24 15:37:02 +0430

    [FIX] url

--- client/src/app/utils/api.utils.ts
+++ client/src/app/utils/api.utils.ts
@@ -1,9 +1,9 @@
-export const BASE_URL = 'http://localhost:5017';
+export const BASE_URL = 'https://localhost:5017';
 
 const INVENTORY_SERVICE = BASE_URL + '/DataInventory';
 export const INVENTORY_IMPORT = INVENTORY_SERVICE + '/Import';
 export const INVENTORY_EXPORT = INVENTORY_SERVICE + '/Export';
-export const INVENTORY_ALL = INVENTORY_SERVICE + '/GetAllFiles';
+export const INVENTORY_ALL = INVENTORY_SERVICE + '/GetFilesInformation';
 
 const PIPELINE_SERVICE = BASE_URL + '/Pipeline';
 export const PIPELINE_CREATE = PIPELINE_SERVICE + '/Create';
