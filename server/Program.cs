1a781fe2d85102d33eb7d0f9670a2698e6766ab1
Ali Bonaftizadeh <alibnz.2001@gmail.com>
2022-08-25 06:44:40 +0430

    Remove commented code

--- server/Program.cs
+++ server/Program.cs
@@ -37,37 +37,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-// using a;
-// using server.Components.Extractors;
-// using server.Components.Loaders;
-// using server.Components.Transformers;
-// using server.Pipelines;
-
-// public static class Program
-// {
-//     public static void Main(string[] args)
-//     {
-//         var dbConfiguration = DBConfigLoader.Load();
-//         var pipeline = new Pipeline(dbConfiguration.Host, dbConfiguration.Username, dbConfiguration.Database,
-//             dbConfiguration.Password);
-//         var extractor = new CSVExtractor(pipeline,
-//             @"C:\Users\Khosro\Desktop\StarAcademy\Summer1401-Project-Team03\server\TestData\input.csv");
-//
-//         // var filter1 = new Filter(pipeline, "location", Operator.Equal, "Iran");
-//         // filter1.PreviousComponents.Add(extractor);
-//         // var filter2 = new Filter(pipeline, "date", Operator.Equal, "2022-08-12");
-//         // filter2.PreviousComponents.Add(filter1);
-//
-//         var selector = new FieldSelector(pipeline, new List<string> {"iso_code", "location"});
-//         selector.PreviousComponents.Add(extractor);
-//
-//         var loader = new CSVLoader(pipeline, selector,
-//             @"C:\Users\Khosro\Desktop\StarAcademy\Summer1401-Project-Team03\server\TestData\output.csv");
-//
-//         pipeline.AddComponent(loader);
-//
-//         pipeline.Execute(2);
-//     }
-// }
