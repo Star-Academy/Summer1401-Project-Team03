a4b69963ea5f80ceba050f96e76bff196955d4ba
Negarnbkhti42 <norob35@gmail.com>
2022-08-27 16:27:28 +0430

    [FEATURE] datalist disabled style

--- client/src/app/components/text-input/text-input.component.ts
+++ client/src/app/components/text-input/text-input.component.ts
@@ -9,6 +9,6 @@ export class TextInputComponent {
     @Input() public label: string = '';
     @Input() public type: string = 'text';
     @Input() public value: string = '';
-    @Input() public disabled: boolean = true;
+    @Input() public disabled: boolean = false;
     @Output() public valueChange = new EventEmitter<string>();
 }
