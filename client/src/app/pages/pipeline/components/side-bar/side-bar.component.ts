33bb48775ffc9ae2f2c4f1e604b42e2df69c1ae8
Negarnbkhti42 <norob35@gmail.com>
2022-08-27 11:35:09 +0430

    [ADD] sidebar empty state

--- client/src/app/pages/pipeline/components/side-bar/side-bar.component.ts
+++ client/src/app/pages/pipeline/components/side-bar/side-bar.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-side-bar',
-  templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.scss']
+    selector: 'app-side-bar',
+    templateUrl: './side-bar.component.html',
+    styleUrls: ['./side-bar.component.scss'],
 })
-export class SideBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SideBarComponent {
+    @Input() public config: string | null = null;
 }
