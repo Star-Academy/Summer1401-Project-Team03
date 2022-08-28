e28faba40b9efd09d70fd6c6694a21e77937d9e3
KhosroMoeini <KhosroMoeini2003@gmail.com>
2022-08-28 17:58:46 +0430

    Add & Remove Added

--- server/components/Component.cs
+++ server/components/Component.cs
@@ -9,6 +9,7 @@ public abstract class Component
     {
         NextComponents = new List<Component>();
         PreviousComponents = new List<Component>();
+        Parameters = new Dictionary<string, List<string>>();
     }
 
     public Component(ComponentInformation information, IQueryBuilder queryBuilder)
@@ -18,6 +19,36 @@ public abstract class Component
         Type = information.Type;
         Parameters = information.Parameters;
         QueryBuilder = queryBuilder;
+    }
+    
+    public void AddNextComponent(Component component)
+    {
+        NextComponents.Add(component);
+    }
+
+    public void RemoveNextComponent(Component component)
+    {
+        if (NextComponents.Remove(component))
+        {
+            return;
+        }
+
+        throw new Exception("next component does not exist!");
+    }
+
+    public void AddPreviousComponent(Component component)
+    {
+        PreviousComponents.Add(component);
+    }
+    
+    public void RemovePreviousComponent(Component component)
+    {
+        if (PreviousComponents.Remove(component))
+        {
+            return;
+        }
+
+        throw new Exception("previous component does not exist!");
     }
 
     public Dictionary<string, List<string>> Parameters { set; get; }
