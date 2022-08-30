c33e416648da6bd754feffbe766992b7df4ffc5c
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-30 11:52:23 +0430

    [ADD] pipeline-board pan gesture

--- client/src/app/pages/pipeline/components/board/board.component.ts
+++ client/src/app/pages/pipeline/components/board/board.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-board',
@@ -11,8 +11,20 @@ export class BoardComponent {
 
     @Input() public gridSize: string = '20px';
 
+    @ViewChild('container') public container!: ElementRef<HTMLDivElement>;
+
+    public mouseIsDown: boolean = false;
+    public spaceIsDown: boolean = false;
+
     public resizeClickHandler(dx: number, dy: number): void {
         this.boardWidth += dx;
         this.boardHeight += dy;
+    }
+
+    public mouseMoveHandler(event: MouseEvent): void {
+        if (this.mouseIsDown && this.spaceIsDown) {
+            this.container.nativeElement.scrollTop -= event.movementY;
+            this.container.nativeElement.scrollLeft -= event.movementX;
+        }
     }
 }
