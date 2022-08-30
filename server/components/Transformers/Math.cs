ce2e485304494ffbbe9f36fe1f9a186393fec697
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2022-08-30 15:04:20 +0430

    supporting multiple entries for math

--- server/components/Transformers/Math.cs
+++ server/components/Transformers/Math.cs
@@ -25,31 +25,39 @@ public class Math : Transformer
     {
         var keys = PreviousComponents[0].GetKeys();
 
-        if (bool.Parse(Parameters[ShouldCreateNewColumn][0]))
-            keys.Add($"{Parameters[FieldsToCalculate][0]}__{Parameters[Values][0]}");
-
+        for (var i = 0; i < Parameters[ShouldCreateNewColumn].Count; i++)
+        {
+            if (bool.Parse(Parameters[ShouldCreateNewColumn][i]))
+                keys.Add($"{Parameters[FieldsToCalculate][i]}__{Parameters[Values][i]}");
+        }
+        
         return keys;
     }
 
     private List<string> GetKeysForQuery()
     {
         var keys = PreviousComponents[0].GetKeys();
-        var valForQuery = GetFunctionFromMathOperator(Parameters[FieldsToCalculate][0], Parameters[Values][0]);
 
-        if (bool.Parse(Parameters[ShouldCreateNewColumn][0]))
-            keys.Add($"{valForQuery} AS {Parameters[FieldsToCalculate][0]}__{Parameters[Values][0]}");
-        else
+        for (var i = 0; i < Parameters[FieldsToCalculate].Count; i++)
         {
-            keys[keys.IndexOf(Parameters[FieldsToCalculate][0])] = $"{valForQuery} AS {Parameters[FieldsToCalculate][0]}";
-        }
+            var valForQuery = GetFunctionFromMathOperator(Parameters[FieldsToCalculate][i], Parameters[Values][i], Parameters[Operators][i]);
 
+            if (bool.Parse(Parameters[ShouldCreateNewColumn][i]))
+                keys.Add($"{valForQuery} AS {Parameters[FieldsToCalculate][i]}__{Parameters[Values][i]}");
+            else
+            {
+                keys[keys.IndexOf(Parameters[FieldsToCalculate][i])] = $"{valForQuery} AS {Parameters[FieldsToCalculate][i]}";
+            }
+
+        }
+        
         return keys;
     }
 
 
-    private string GetFunctionFromMathOperator(string x, string y)
+    private string GetFunctionFromMathOperator(string x, string y, string @operator)
     {
-        return Parameters[Operators][0].GetOperatorType() switch
+        return @operator.GetOperatorType() switch
         {
             MathOperator.Log => $"LOG({x}, {y})",
             MathOperator.Sin => $"SIN({x})",
