d4eebf1f6476e87ba1272441e4ffafb5947708e9
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-30 10:17:17 +0430

    [ADD] cache sideBarShown,bottomBarShown

--- client/src/app/pages/pipeline/pipeline.component.ts
+++ client/src/app/pages/pipeline/pipeline.component.ts
@@ -6,6 +6,21 @@ import {Component} from '@angular/core';
     styleUrls: ['./pipeline.component.scss'],
 })
 export class PipelineComponent {
-    public sideBarShown: boolean = true;
-    public bottomBarShown: boolean = true;
+    public get sideBarShown(): boolean {
+        const cached = localStorage.getItem('sideBarShown');
+        return (cached || 'true') === 'true';
+    }
+
+    public set sideBarShown(newValue: boolean) {
+        localStorage.setItem('sideBarShown', String(newValue));
+    }
+
+    public get bottomBarShown(): boolean {
+        const cached = localStorage.getItem('bottomBarShown');
+        return (cached || 'true') === 'true';
+    }
+
+    public set bottomBarShown(newValue: boolean) {
+        localStorage.setItem('bottomBarShown', String(newValue));
+    }
 }
